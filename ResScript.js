340f41b0e567e1777d5b73c4e9d9e5da8a7cd246
Jason Graziano <mrehawk82@gmail.com>
2015-05-26 11:32:12 -0400

    Update ResScript.js

--- ResScript.js
+++ ResScript.js
@@ -13,23 +13,24 @@
                              "<br /> Notable Achievements",
                              "Won 1st place in Niagara Orleans BOCES VICA competition in 2000 for Drywall and Electric. <br /> Helped Construct a 49,000 cu yard concrete parking apron for the Navy in Kuwait, Earned Battle E ribbon. <br /> Earned Good Conduct Service medal and made the rank of Builder Third Class Petty Officer. <br /> Raising 4 kind-hearted children, one of my most proudest of accomplishments.",
                              "<br /> References",
-                             "Walter Ritchart, personal friend.  716-715-9867 <br /> John Haugeland, Personal Friend, johnhaugeland@yahoo.com <br /> Robert Graziano, Father, 716-990-5251",
+                             "Available upon request",
                              "<br /> Skills",
-                             "Firm understanding of multiple engineering disciplines.  Concrete, Masonry, Carpentry, Plumbing, Electric. <br /> Basic understaning of HTML5, CSS, and JavaScript(JSON, Jquery, nodeJS )",
+                             "Firm understanding of multiple engineering disciplines.  Concrete, Masonry, Carpentry, Plumbing, Electric. <br /> Basic understanding of HTML5, CSS, and JavaScript(JSON, Jquery, raw JS )",
                              "<br /> Comments & Miscellaneous",
                              "I am looking to learn and never stop learning.  I believe I am a good team player and work well with others.  I am willing to relocate to anywhere that can help me develop in to an expert programmer.  Currently I am trying to master JavaScript.  I know and understand Jquery, however my personal intentions are to master JS first before using other scripts. I can use JSON to store objects to local storage properly.  I know how to dynamically add HTML through JS rather than storing the elements in the HTML DOM. I am aware that I have a lot to learn, and I am humble about it.",
                              "Portfolio",
-                             "<a href='http://www.ehawk.us/fictionary.html' target='_blank'>Fictionary</a> is a jquery powered information tool for mythology.  I hope to develop a JavaScript tool to help this grow faster <br /> <a href='http://www.ehawk.us/Graz.html' target='_blank'>ToDo App</a> is a app that uses local storage and JSON to save and display items. <br /> <a href='http://www.ehawk.us/Timer.html' target='_blank'>Timer</a> is an app I made to familiarize myself with timeouts and math using the date object. <br /> <a href='http://www.ehawk.us/home1.html' target='_blank'>First To Do app</a> I made to familiarize myself with HTML5 and CSS using Javascript for functionality. <br /> "
+                             "<a href='http://www.ehawk.us/fictionary.html' target='_blank'>Fictionary</a> is a jquery powered information tool for mythology.  I hope to develop a JavaScript tool to help this grow faster <br /> <a href='http://www.ehawk.us/Graz.html' target='_blank'>ToDo App</a> is a app that uses local storage and JSON to save and display items. <br /> <a href='http://www.ehawk.us/Timer.html' target='_blank'>Timer</a> is an app I made to familiarize myself with timeouts and math using the date object. <br /> <a href='http://www.ehawk.us/home1.html' target='_blank'>First To Do app</a> I made to familiarize myself with HTML5 and CSS using Javascript for functionality. <br /> <a href='https://github.com/Ehawk82' target='_blank'>GitHub</a> account",
+                             "This resume was created by Jason Graziano for the purpose of seeking meaningful employment."
                             ];
                 Elems = "<div id='headWrap'><h3 id='heading'></h3><span id='headInfo'></span></div>";
-                Elems += "<div id='wrapper'><div id='div1'><h4 id='h4div1'></h4><div id='div1Info'></div></div>";
-                Elems += "<div id='div2'><h4 id='h4div2'></h4><div id='div2Info'></div></div>";
-                Elems += "<div id='div3'><h4 id='h4div3'></h4><div id='div3Info'></div></div>";
-                Elems += "<div id='div4'><h4 id='h4div4'></h4><div id='div4Info'></div></div>";
-                Elems += "<div id='div5'><h4 id='h4div5'></h4><div id='div5Info'></div></div>";
-                Elems += "<div id='div6'><h4 id='h4div6'></h4><div id='div6Info'></div></div>";
-                Elems += "<div id='div7'><h4 id='h4div7'></h4><div id='div7Info'></div></div>";
-                Elems += "<div id='footer'></div></div>";
+                Elems += "<div id='wrapper'><div id='div1'><h4 id='h4div1' class='h4C'></h4><div id='div1Info' class='infoC'></div></div>";
+                Elems += "<div id='div2'><h4 id='h4div2' class='h4C'></h4><div id='div2Info' class='infoC'></div></div>";
+                Elems += "<div id='div3'><h4 id='h4div3' class='h4C'></h4><div id='div3Info' class='infoC'></div></div>";
+                Elems += "<div id='div4'><h4 id='h4div4' class='h4C'></h4><div id='div4Info' class='infoC'></div></div>";
+                Elems += "<div id='div5'><h4 id='h4div5' class='h4C'></h4><div id='div5Info' class='infoC'></div></div>";
+                Elems += "<div id='div6'><h4 id='h4div6' class='h4C'></h4><div id='div6Info' class='infoC'></div></div>";
+                Elems += "<div id='div7'><h4 id='h4div7' class='h4C'></h4><div id='div7Info' class='infoC'></div></div>";
+                Elems += "<div id='footer'><h6 id='h6Footer'></h6></div></div>";
                 body.innerHTML = Elems;
                 document.getElementById("heading").innerHTML = textElems[0];
                 document.getElementById("headInfo").innerHTML = textElems[1];
@@ -47,20 +48,10 @@
                 document.getElementById("div6Info").innerHTML = textElems[13];
                 document.getElementById("h4div7").innerHTML = textElems[14];
                 document.getElementById("div7Info").innerHTML = textElems[15];
-            },
-            loadStyle: function () {
-                var header, styleSheet;
-                header = document.getElementsByTagName('head')[0];
-                styleSheet = document.createElement('link');
-                styleSheet.rel = 'stylesheet';
-                styleSheet.type = 'text/css';
-                styleSheet.href = 'mainStyle.css';
-                styleSheet.media = 'all';
-                header.appendChild(styleSheet);
+                document.getElementById("h6Footer").innerHTML = textElems[16];
             }
         };
     window.onload = function () {
         runUI.loadData();
-        runUI.loadStyle();
     };
 }());
